3e2279a76ce528028518b9588c3d85acbddc0e4e
Artiom Ciobanu <aciobanumain@gmail.com>
2021-07-13 15:57:08 +0300

    Added JwtConfigurator.cs

---
+++ WebApiBaseLibrary.Authorization/Configurators/JwtConfigurator.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using WebApiBaseLibrary.Authorization.Models;
+
+namespace WebApiBaseLibrary.Authorization.Configurators
+{
+    public class JwtConfigurator : IJwtConfigurator
+    {
+        public JwtConfiguration Configuration { get; }
+
+        public JwtConfigurator(JwtConfiguration jwtConfiguration)
+        {
+            Configuration = jwtConfiguration;
+        }
+
+        public TokenValidationParameters ValidationParameters => new()
+        {
+            ValidateIssuer = true,
+            ValidIssuer = Configuration.Issuer,
+            ValidateAudience = true,
+            ValidAudience = Configuration.Audience,
+            ValidateLifetime = true,
+            RequireExpirationTime = true,
+            IssuerSigningKey = GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true
+        };
+
+        public SigningCredentials GetSigningCredentials() =>
+            new(GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256Signature);
+
+        private SymmetricSecurityKey GetSymmetricSecurityKey() => new(GetByteKey());
+        private byte[] GetByteKey() => Encoding.UTF8.GetBytes(Configuration.SecretKey);
+    }
+}
