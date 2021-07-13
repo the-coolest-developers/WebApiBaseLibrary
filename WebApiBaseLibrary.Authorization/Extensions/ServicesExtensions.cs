b5e0ed4c165b8a068050de10278f64d4505af9d7
Artiom Ciobanu <aciobanumain@gmail.com>
2021-07-13 16:06:11 +0300

    Added ServicesExtensions.cs

---
+++ WebApiBaseLibrary.Authorization/Extensions/ServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApiBaseLibrary.Authorization.Constants;
+using WebApiBaseLibrary.Authorization.Models;
+
+namespace WebApiBaseLibrary.Authorization.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void AddSingletonJwtConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtConfiguration = configuration.GetSection(Appsettings.JwtConfiguration)
+                .Get<JwtConfiguration>();
+
+            services.AddSingleton(_ => jwtConfiguration);
+        }
+    }
+}
