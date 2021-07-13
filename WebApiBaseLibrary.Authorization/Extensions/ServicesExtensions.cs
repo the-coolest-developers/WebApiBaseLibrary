15ebaf20f476f80555491491ec565f709313dafa
Artiom Ciobanu <aciobanumain@gmail.com>
2021-07-13 16:10:47 +0300

    Added AddConfiguredJwtBearer method

--- WebApiBaseLibrary.Authorization/Extensions/ServicesExtensions.cs
+++ WebApiBaseLibrary.Authorization/Extensions/ServicesExtensions.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApiBaseLibrary.Authorization.Configurators;
 using WebApiBaseLibrary.Authorization.Constants;
 using WebApiBaseLibrary.Authorization.Models;
 
@@ -7,12 +11,29 @@ namespace WebApiBaseLibrary.Authorization.Extensions
 {
     public static class ServicesExtensions
     {
-        public static void AddSingletonJwtConfiguration(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddSingletonJwtConfiguration(
+            this IServiceCollection services,
+            IConfiguration configuration)
         {
             var jwtConfiguration = configuration.GetSection(Appsettings.JwtConfiguration)
                 .Get<JwtConfiguration>();
 
-            services.AddSingleton(_ => jwtConfiguration);
+            return services.AddSingleton(_ => jwtConfiguration);
         }
+
+        public static AuthenticationBuilder AddConfiguredJwtBearer(
+            this IServiceCollection services,
+            IServiceProvider serviceProvider)
+            => services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                var jwtConfigurator = serviceProvider.GetService<IJwtConfigurator>();
+
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = jwtConfigurator?.ValidationParameters;
+            });
     }
 }
