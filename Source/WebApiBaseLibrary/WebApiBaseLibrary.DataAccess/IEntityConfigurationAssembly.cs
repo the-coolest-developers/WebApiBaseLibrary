d474b3fb4256702089bc6bde00d64575dfb18b4b
Artiom Ciobanu <Artiom.Ciobanu@endava.com>
2021-08-16 10:49:33 +0300

    [TGS-64] Added BaseDbContext

---
+++ Source/WebApiBaseLibrary/WebApiBaseLibrary.DataAccess/IEntityConfigurationAssembly.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace WebApiBaseLibrary.DataAccess
+{
+    public interface IEntityConfigurationAssembly
+    {
+        Assembly GetEntityConfigurationAssembly();
+    }
+}
