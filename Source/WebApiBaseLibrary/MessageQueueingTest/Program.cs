a3aebeed49c1af9175125cda5f840ffb34010a12
Artiom Ciobanu <aciobanumain@gmail.com>
2021-09-05 11:20:58 +0300

    [TGS-67] Added Test project

---
+++ Source/WebApiBaseLibrary/MessageQueueingTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using WebApiBaseLibrary.Infrastructure.MessageQueueing;
+using WebApiBaseLibrary.Infrastructure.MessageQueueing.RabbitMQ;
+
+namespace MessageQueueingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessageQueueConnectionFactory factory = new RabbitMQConnectionFactory("localhost");
+            var connection = factory.CreateConnection();
+            var reader = connection.CreateReader("LeaveGameQueue");
+
+            StartReading(reader);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        static void Received(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void StartReading(IMessageQueueReader reader)
+        {
+            reader.SetReceivedAction(Received);
+            reader.StartReading();
+        }
+    }
+}
