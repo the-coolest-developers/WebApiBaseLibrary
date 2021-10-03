7b91e108aac6be5aa3c9680604964a563cb1bfeb
Artiom Ciobanu <Artiom.Ciobanu@endava.com>
2021-10-03 09:33:39 +0300

    [TGS-69] AddRabbitMQ method doesn't need IConfiguration anymore

--- Source/WebApiBaseLibrary/WebApiBaseLibrary.Infrastructure/Extensions/RabbitMQ/ServiceCollectionExtensions.cs
+++ Source/WebApiBaseLibrary/WebApiBaseLibrary.Infrastructure/Extensions/RabbitMQ/ServiceCollectionExtensions.cs
@@ -1,38 +1,29 @@
-﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WebApiBaseLibrary.Infrastructure.Configuration;
 using WebApiBaseLibrary.Infrastructure.Constants;
-using WebApiBaseLibrary.Infrastructure.Extensions;
+using WebApiBaseLibrary.Infrastructure.MessageQueueing;
+using WebApiBaseLibrary.Infrastructure.MessageQueueing.RabbitMQ;
 
-namespace WebApiBaseLibrary.Infrastructure.MessageQueueing.RabbitMQ.Extensions
+namespace WebApiBaseLibrary.Infrastructure.Extensions.RabbitMQ
 {
     public static class ServiceCollectionExtensions
     {
         [SuppressMessage("ReSharper", "InconsistentNaming")]
         public static IServiceCollection AddRabbitMQ(
             this IServiceCollection services,
-            IConfiguration configuration,
-            Func<IServiceProvider> serviceProviderFunc)
+            IConfiguration configuration)
         {
             var rabbitMQConfiguration = configuration
                 .GetSection(InfrastructureAppSettings.RabbitMQConfiguration)
                 .Get<RabbitMQConfiguration>();
 
-            services.AddSingleton<IMessageQueueConnectionFactory, RabbitMQConnectionFactory>(_ =>
-            {
-                var connectionFactory = new RabbitMQConnectionFactory(rabbitMQConfiguration?.HostName);
-
-                return connectionFactory;
-            });
-            services.AddScoped<IMessageQueueConnection, RabbitMQConnection>(_ =>
-            {
-                var connectionFactory = serviceProviderFunc().GetService<IMessageQueueConnectionFactory>();
-                var connection = connectionFactory?.CreateConnection();
-
-                return (RabbitMQConnection) connection;
-            });
+            var connectionFactory = new RabbitMQConnectionFactory(rabbitMQConfiguration?.HostName);
+            var connection = connectionFactory?.CreateConnection();
+                
+            services.AddSingleton<IMessageQueueConnectionFactory, RabbitMQConnectionFactory>(_ => connectionFactory);
+            services.AddSingleton<IMessageQueueConnection, RabbitMQConnection>(_ => (RabbitMQConnection) connection);
 
             return services;
         }
--- Source/WebApiBaseLibrary/WebApiBaseLibrary.Infrastructure/MessageQueueing/RabbitMQ/RabbitMQConnection.cs
+++ Source/WebApiBaseLibrary/WebApiBaseLibrary.Infrastructure/MessageQueueing/RabbitMQ/RabbitMQConnection.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using RabbitMQ.Client;
 
 namespace WebApiBaseLibrary.Infrastructure.MessageQueueing.RabbitMQ
