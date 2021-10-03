223434aadb46a7b11d17ef2d5a3feb9b544f71bb
Artiom Ciobanu <Artiom.Ciobanu@endava.com>
2021-10-03 15:29:31 +0300

    [TGS-69] Added MQ Attributes

---
+++ Source/WebApiBaseLibrary/WebApiBaseLibrary.Infrastructure/Extensions/RabbitMQ/ApplicationBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using WebApiBaseLibrary.Infrastructure.Attributes;
+using WebApiBaseLibrary.Infrastructure.MessageQueueing;
+using WebApiBaseLibrary.Infrastructure.Reflection;
+
+namespace WebApiBaseLibrary.Infrastructure.Extensions.RabbitMQ
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseMessageQueueingServices(this IApplicationBuilder app)
+        {
+            var messageQueueServices = ReflectionHelper
+                .GetAllTypesWithAttribute<MessageQueueServiceAttribute>()
+                .ToArray();
+
+            var connection = app.ApplicationServices.GetService<IMessageQueueConnection>();
+
+            foreach (var messageQueueServiceType in messageQueueServices)
+            {
+                var methodsWithAttribute =  messageQueueServiceType
+                    .GetMethods()
+                    .Where(m => m.GetCustomAttributes<MessageQueueActionAttribute>().Any());
+
+                var messageQueueServiceActionMethods = methodsWithAttribute
+                    .ToDictionary(
+                        m => m.GetCustomAttribute<MessageQueueActionAttribute>()?.QueueName,
+                        m => m);
+
+                foreach (var (queueName, method) in messageQueueServiceActionMethods)
+                {
+                    var reader = connection?.CreateReader(queueName);
+
+                    var parameter = method.GetParameters().FirstOrDefault();
+                    var parameterType = parameter?.ParameterType;
+
+                    reader?.SetReceivedAction(message =>
+                    {
+                        var serviceInstance = app.ApplicationServices.GetService(messageQueueServiceType);
+
+                        var deserializedMessage = JsonConvert.DeserializeObject(message, parameterType!);
+
+                        method.Invoke(serviceInstance, new[] {deserializedMessage});
+                    });
+                    reader?.StartReading();
+                }
+            }
+
+            return app;
+        }
+    }
+}
---
+++ Source/WebApiBaseLibrary/WebApiBaseLibrary.Infrastructure/Reflection/ReflectionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace WebApiBaseLibrary.Infrastructure.Reflection
+{
+    public static class ReflectionHelper
+    {
+        public static IEnumerable<Type> GetAllTypesWithAttribute<TAttribute>()
+            where TAttribute : Attribute
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var types = assembly.GetTypes().Where(t => t.GetCustomAttribute<TAttribute>() != null);
+
+            return types;
+        }
+    }
+}
